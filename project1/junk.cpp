2fb0a599fc11cc815d2101a7078a474b080a1a5a
Robert Geil <robert.geil999@gmail.com>
2019-04-17 21:15:41 -0700

    Tested and updated some items for Ubuntu support

---
+++ project1/junk.cpp
@@ -0,0 +1,29 @@
+//junk.cpp
+// This file generates an arbitrary number of random bytes
+// into a specified file. Useful for generating large files
+// for transmission across the server
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+    if(argc != 3){
+        cerr <<"Enter arguments in the form ./junk [filename] [bytes]"<<endl;
+        exit(1);
+    }
+    ofstream output;
+    output.open(argv[1], ios::binary);
+    int filelength = atoi(argv[2]);
+    cout<<"File length: "<<filelength<<endl;
+    char byte;
+    srand(time(0));
+    for(int i = 0; i<filelength; i++){
+        byte = rand()%sizeof(char);
+        output << byte;
+    }
+    output.flush();
+    output.close();
+}
